c25c84e2a205b8dc2065975e4ec2cdd6e7369ee5
Ben Evans <ben_r_evans@hotmail.com>
2023-12-31 13:17:40 +0000

    minor solution updates

--- src/Toess.WebApi/obj/Debug/net8.0/Toess.WebApi.AssemblyInfo.cs
+++ src/Toess.WebApi/obj/Debug/net8.0/Toess.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toess.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c8f490d6056c1fdc298880b6d8fe4a83e9c690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toess.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toess.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
