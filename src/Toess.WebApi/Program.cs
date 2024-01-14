638f23acc3ca02a26cd9852610deb9747733eed0
Ben Evans <ben_r_evans@hotmail.com>
2024-01-14 14:22:30 +0000

    Added Honeycomb

--- src/Toess.WebApi/Program.cs
+++ src/Toess.WebApi/Program.cs
@@ -1,4 +1,43 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string serviceName = "WebApi";
+
+var honeycombOptions = builder.Configuration.GetHoneycombOptions();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddConsoleExporter();
+});
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddHoneycomb(honeycombOptions)
+        .AddCommonInstrumentations()
+        // .AddAspNetCoreInstrumentation()
+        // .AddHttpClientInstrumentation()
+        
+        // .AddOtlpExporter(option =>
+        // {
+        //     option.Endpoint = new Uri("https://api.eu1.honeycomb.io/v1/traces");
+        //     option.Headers = $"x-honeycomb-team=zGIc4yXnFfasp7M5twLvED";
+        //     option.Protocol = OtlpExportProtocol.HttpProtobuf;
+        // })
+        .AddConsoleExporter())
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+    );
+builder.Services.AddSingleton(TracerProvider.Default.GetTracer(honeycombOptions.ServiceName));
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,8 +60,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (Tracer tracer) =>
 {
+    using var span = tracer.StartActiveSpan("app.manual-span");
+    span.SetAttribute("app.manual-span.message", "Adding custom spans is also super easy!");
+    
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
